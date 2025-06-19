4ee4e1d39aba357afc2ebffbf623756610e62155
liqidecg <128770480+liqidecg@users.noreply.github.com>
2025-06-19 16:19:20 +0800

    Update Toast.kt

--- app/src/main/java/com/android/hmal/ui/util/Toast.kt
+++ app/src/main/java/com/android/hmal/ui/util/Toast.kt
@@ -1,8 +1,8 @@
-package com.android.hmal.ui.util
+package com.android.lbe.ui.util
 
 import android.widget.Toast
 import androidx.annotation.StringRes
-import com.android.hmal.sysApp
+import com.android.lbe.sysApp
 
 fun makeToast(@StringRes resId: Int) {
     Toast.makeText(sysApp, resId, Toast.LENGTH_SHORT).show()

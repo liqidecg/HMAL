bcf59e163b10adeeae830d6c4882de65253896ba
liqidecg <128770480+liqidecg@users.noreply.github.com>
2025-06-19 17:47:15 +0800

    Update PmsHookTarget33.kt

--- xposed/src/main/java/com/android/lbe/xposed/hook/PmsHookTarget33.kt
+++ xposed/src/main/java/com/android/lbe/xposed/hook/PmsHookTarget33.kt
@@ -10,7 +10,7 @@ import com.android.lbe.xposed.*
 import java.util.concurrent.atomic.AtomicReference
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-class PmsHookTarget33(private val service: HMALService) : IFrameworkHook {
+class PmsHookTarget33(private val service: lbeService) : IFrameworkHook {
 
     companion object {
         private const val TAG = "PHT33"
